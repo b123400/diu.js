a4f427a8acfe76d0d788b9c717cfcc51e17ced84
b123400 <b123400@gmail.com>
2014-06-12 17:38:24 +0900

    Filter non-English in methodName so can swear more flexibly Fixed syntax error

--- diu.js
+++ diu.js
@@ -2,11 +2,13 @@
 	global.屌 = function( obj, methodName ){
 		if( !methodName ){
 			return obj;
-		} else if ( typeof obj[methodName] === "function" ){
+		}
+		methodName = methodName.replace(/[^a-zA-Z0-9\-]/g,'')
+		if ( typeof obj[methodName] === "function" ){
 			return obj[methodName].bind(obj);
 		}
 		return obj[methodName];
-	}
+	};
 
 	['老味','鳩','啦','仆街'].forEach( function(valName){
 		Object.defineProperty( Object.prototype, valName,{
